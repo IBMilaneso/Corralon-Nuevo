863d8dc0bed10fe6a8833c1e3b9f8faa9fc4cd81
IBMilaneso <168839602+IBMilaneso@users.noreply.github.com>
2025-12-12 19:37:17 -0600

    Alo

--- frontend/src/components/SobreNosotros.vue
+++ frontend/src/components/SobreNosotros.vue
@@ -3,7 +3,7 @@
     <div class="about-card">
       <div class="image-container">
         <img 
-          src="C:\Users\jevus\Downloads\MyFlow\1764536145542.jpg" 
+          src="C:\Users\jevus\Downloads\MyFlow\Snapchat-1443653098.jpg"
           alt="Equipo Corralón"
         >
         <div class="overlay"></div>
